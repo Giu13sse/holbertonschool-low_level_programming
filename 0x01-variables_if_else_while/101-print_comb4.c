b4e783e2be5104690d06e1cae77657fb9c0a7e6e
Giu13sse <Giussedipietro@gmail.com>
2022-02-14 16:42:00 -0800

    extra combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int left; 
+int right;
+int center;
+
+/*for 0-9 with ASCII*/
+
+for (left = 48; left <= 57; left++)
+{
+for (center = left + 1; center <= 57; center++)
+{
+for (right = center + 1 ; right <= 57; right++)
+
+{
+
+putchar(left); 
+putchar (center);
+putchar (right);
+
+/*break program*/
+if ((left == 55) && (center == left + 1) && (right == center + 1))
+{
+break; 
+}
+
+putchar(','); 
+putchar (' '); 
+
+} 
+
+} 
+
+} 
+
+putchar('\n'); 
+
+return (0);
+
+}
