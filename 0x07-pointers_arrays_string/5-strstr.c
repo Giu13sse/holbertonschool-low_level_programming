5e7d5e66dbf7faa3fffa17bf9589026c6827b696
Giu13sse <Giussedipietro@gmail.com>
2022-02-28 20:00:06 -0800

    betty overlap

--- 0x07-pointers_arrays_string/5-strstr.c
+++ 0x07-pointers_arrays_string/5-strstr.c
@@ -27,11 +27,11 @@ do {
 if (needle[index + 1] == '\0')
 return (haystack);
 index++;
-} 
-while (haystack[index] == needle[index]);
+}
+while (haystack[index] ==
+needle[index])
 }
 haystack++;
 }
-
 return ('\0');
 }
