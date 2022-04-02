1f0407a1db6f29492a3817628379c72d2818b50a
Giu13sse <Giussedipietro@gmail.com>
2022-04-01 18:55:58 -0700

    102

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,11 +5,10 @@
  * @head: points to start of list
  * Return: number of nodes in size_t or exit 98
  */
-size_t print_listint_safe(const listint_t *head)
+size_t print_listint(const listint_t *head)
 {
-/* declarations */
+
 size_t nodeCount = 0;
-/* check for null pointer */
 while (head)
 {
 printf("[%p] %d\n", (void *)head, head->n);
