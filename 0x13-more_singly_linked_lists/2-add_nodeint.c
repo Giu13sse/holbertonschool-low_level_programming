5712fc5a23c471033d86c9593f8ef8004618d3cc
Giu13sse <Giussedipietro@gmail.com>
2022-04-01 18:24:18 -0700

    3

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -21,26 +21,3 @@ new_node->next = *head;
 
 return (*head);
 }
-#include "lists.h"
-
-/**
- * add_nodeint - add new node a beginning of a a list
- * @head: pointer
- *@n:number
-* Return:  number elements of linked list
-*/
-
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-listint_t *new_node;
-new_node = malloc(sizeof(listint_t));
-
-if (new_node == NULL)
-return (NULL);
-
-new_node->n = n;
-new_node->next = *head;
-*head = new_node;
-
-return (*head);
-}
