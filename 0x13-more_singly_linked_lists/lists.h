b70e4c8c5c2340a550f9323867f64a15dd88af73
Giu13sse <Giussedipietro@gmail.com>
2022-04-01 19:05:29 -0700

    101

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,6 +35,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+size_t looped_listint_len(const listint_t *head);
 int _putchar(char c);
 
 #endif
