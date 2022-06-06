f973424216893bcbf79bd82cdfd0a71d5f7e9eb8
giu13sse <giussedipietro@gmail.com>
2022-06-06 16:52:10 -0700

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,54 +1,49 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_set - function that adds an element to hash table
- * @ht: hash table to update
- * @key: key
- * @value: value of key
- * Return: updated table
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hashs table I want to add or update the key/value to
+ * @key: is the str passed to djb2 (tha key )
+ * @value: value associated with the key
+ * Return: 1 if succeeded or 0 if fail
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx = 0;
-	char *temp_val = NULL;
-	hash_node_t *temp = NULL;
-	hash_node_t *new = NULL;
+	hash_node_t *hash_nodes;
+	hash_node_t *new_node;
+	unsigned long int index;
 
-	if (ht == NULL || ht->array == NULL || value == NULL)
+	if (!ht || !key)
 		return (0);
-
-	if (strlen(key) == 0 || key == NULL)
-		return (0);
-	temp_val = strdup(value);
-	if (temp_val == NULL)
-		return (0);
-	idx = key_index((unsigned char *)key, ht->size);
-
-	/* Collision checker */
-	temp = ht->array[idx];
-	while (temp)
+	/* first I need to know where must be indexed the new node in the ht */
+	index = key_index((const unsigned char *)key, ht->size);
+	/* hashnodes points to the array of the ht with the respective idx */
+	hash_nodes = ht->array[index];
+	/* in case of key/value update */
+	while (hash_nodes)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (strcmp(hash_nodes->key, key) == 0)
 		{
-			free(temp->value);
-			temp->value = temp_val;
-			temp->value = strdup(value);
-			free(temp_val);
+			/* delete the actual value in the hash table */
+			free(hash_nodes->value);
+			/* the passed value must be duplicated, task cond. */
+			hash_nodes->value = strdup(value);
+			if (hash_nodes->value == NULL)
+				return (0);
 			return (1);
 		}
-		temp = temp->next;	
+		hash_nodes = hash_nodes->next;
 	}
-
-	/* If no collision, insert node */
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(new);
+	/* in case of adding a new node to the hash table */
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
 		return (0);
-	}
-	new->key = strdup(key);
-	new->value = temp_val;
-	new->next = ht->array[idx];
-	ht->array[idx] = new;
+	/* copy of key because is a constant and can assing it  */
+	new_node->key = strdup(key);
+	/* the passed value must be duplicated, task cond. */
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+		return (0);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
 }
