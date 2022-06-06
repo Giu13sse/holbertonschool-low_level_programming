672c673870f9dee915158da02d8b1ef74011c444
giu13sse <giussedipietro@gmail.com>
2022-06-06 16:53:53 -0700

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,30 +1,29 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_get - function that retrieves a value by key
- * @ht: hash table
- * @key: key to look for
- * Return: associated value, or NULL if key not found
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: is the hash table I want to look into
+ * @key: is the key I'm looking for in the hash table
+ * Return: the value associated with the key or NULL if key is not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int idx = 0;
-	hash_node_t *temp = NULL;
+	unsigned long int index;
+	hash_node_t *hash_nodes = NULL;
 
-	if (key == NULL)
+	if (!ht || !key)
 		return (NULL);
 
-	if (ht != NULL)
+	index = key_index((const unsigned char *)key, ht->size);
+	hash_nodes = ht->array[index];
+	while (hash_nodes != NULL)
 	{
-		idx = key_index((const unsigned char *)key, ht->size);
-		temp = ht->array[idx];
-		/* Look for key and return value if found */
-		while (temp != NULL)
-		{
-			if (strcmp(temp->key, key) == 0)
-				return (temp->value);
-			temp = temp->next;
-		}
+		/* if the passed key is in the hash table*/
+		/* return the value in in the found key on the hash table */
+		if (strcmp(hash_nodes->key, key) == 0)
+			return (hash_nodes->value);
+		/* otherwise keeps looking */
+		hash_nodes = hash_nodes->next;
 	}
+	/* if the index is not found cuz the ley doesn't exists */
 	return (NULL);
 }
