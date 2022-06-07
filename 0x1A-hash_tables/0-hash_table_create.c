5e6a212afe483683f4c7f3963b9dab959a0c9343
giu13sse <giussedipietro@gmail.com>
2022-06-06 17:04:02 -0700

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,26 +2,31 @@
 
 /**
  * hash_table_create - creates a hash table
- * @size: the size of the hash table
+ * @size: size of the hash table
  *
- * Return: pointer to hash table type
+ * Return: pointer to the newly created hast table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
 
-	if (!size)
-		return (NULL);
-
-	ht = calloc(1, sizeof(hash_table_t));
-	if (!ht)
-		return (NULL);
-	ht->size = size;
-	ht->array = calloc(size, sizeof(hash_node_t *));
-	if (!ht->array)
+	if (hash_table == NULL)
 	{
-		free(ht);
+		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
 	}
-	return (ht);
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+	{
+		fprintf(stderr, "Error: malloc failes\n");
+		free(hash_table);
+		return (NULL);
+	}
+	for (; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+	return (hash_table);
 }
